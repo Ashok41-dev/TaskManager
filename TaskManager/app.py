09b0f9887561bbf12d17158750ac52f0c75e4fe5
ashok Reddy <kurriashokreddy100@gmail.com>
2024-11-25 19:14:50 +0530

    sidebar_somebackendchanges

--- TaskManager/app.py
+++ TaskManager/app.py
@@ -1,16 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
+import sqlite3
 
-
+db = sqlite3.connect('taskmanager.db') 
 
 app=Flask(__name__)
 
 
-
 @app.route('/')
 def HomePage():
+
     return render_template('index.html')
 
+@app.route('/fetch')
+def FetchDetails():
+    tasks=db.execute('Select * from tasks').fetchall()
+    return jsonify({"tasks":tasks})
 
+@app.route('/createTask')
+def FetchDetails():
+    return render_template('index.html')
+
+@app.route('/delete/:ID',methods=['DELETE'])
+def DeleteTask():
+    return render_template('index.html')
+
+@app.route('/update/:ID',methods=['UPDATE'])
+def DeleteTask():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
